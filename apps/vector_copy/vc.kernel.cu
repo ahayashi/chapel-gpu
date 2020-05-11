e43db1b2b7c9ef626da61dd12926dbbe178cf530
Akihiro Hayashi <ahayashi@gatech.edu>
2020-05-11 12:56:18 -0400

    Create a dedicated header for call_gpu_functor

--- apps/vector_copy/vc.kernel.cu
+++ apps/vector_copy/vc.kernel.cu
@@ -6,23 +6,7 @@ __global__ void vc(float *dA, float *dB, int N) {
     }
 }
 #else
-template<typename functor_type>
-static __global__ void driver_kernel(functor_type functor, unsigned niters) {
-    const int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < niters) {
-        functor(tid);
-    }
-}
-
-template <typename functor_type>
-inline void call_gpu_functor(unsigned niters, unsigned tile_size,
-        cudaStream_t stream, functor_type functor) {
-    //functor_type *actual = (functor_type *)functor;
-
-    const unsigned block_size = tile_size;
-    const unsigned nblocks = (niters + block_size - 1) / block_size;
-    driver_kernel<<<nblocks, block_size, 0, stream>>>(functor, niters);
-}
+#include "lambda.h"
 #endif
 
 extern "C" {
