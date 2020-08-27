d875652467a7d274ea8f0591d35847b023038fe6
Akihiro Hayashi <ahayashi@gatech.edu>
2020-08-27 16:46:07 -0400

    Update MM

--- apps/mm/mm.cu
+++ apps/mm/mm.cu
@@ -65,7 +65,7 @@ __global__ void mm(float *dA, float *dB, float *dC, int DIM, int N, int GPUN) {
         for (int k = 0; k < DIM; k++) {
             sum += dA[i*DIM+k] * dB[k*DIM+j];
         }
-        dC[id] += sum;
+        dC[id] = sum;
     }
 }
 
